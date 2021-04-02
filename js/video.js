b77254c627d86cf10f0ab4853fd28bb85af432bd
cyn-ding <71353249+cyn-ding@users.noreply.github.com>
2021-04-01 20:51:14 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,64 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML=video.volume*100+"%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate = video.playbackRate *0.95;
+	video.playbackRate *=.95
+	console.log("New speed is "+video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate = video.playbackRate /.95;
+	video.playbackRate /=.95
+	console.log("New speed is "+video.playbackRate)
+});
+
+// document.querySelector("#slider").addEventListener("click", function() {
+// 	console.log("Changing Volume");
+// 	console.log(this.value)
+// 	video.volume=this.value/100
+// 	document.querySelector("#volume").innerHTML=video.volume*100+"%";
+// });
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+	if (video.muted === false){
+		video.muted=true;
+	} else {
+		video.muted=false;
+	}
+});
+
+// document.querySelector("#skip").addEventListener("click", function() {
+// 	console.log("Skip Ahead");
+// 	video.currentTime(video.currentTime() + 10);
+// });
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Changing CSS");
+	video.play()
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Changing CSS");
+	video.play()
+	video.classList.remove("oldSchool");
+});
